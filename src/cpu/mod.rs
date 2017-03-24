032dd77fcb53b7e1ad2a91048ac004deb905da3c
Martin Lindhe <martin-commit@ubique.se>
2017-03-24 14:58:43 +0100

    implement 8bit registers

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,7 +1,21 @@
+#![allow(dead_code)]
+
 pub struct CPU {
     pub pc: u16,
     memory: [u8; 0x10000],
-    r16: [u16; 8], // XXX instead use a register struct, which has 8-bit parts adressable so we can set AL to a value and AX gets it too
+    r16: [Register16; 8],
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Register16 {
+    hi: u8,
+    lo: u8,
+}
+
+impl Register16 {
+    fn u16(&self) -> u16 {
+        (self.hi as u16) << 8 | self.lo as u16
+    }
 }
 
 // r16
@@ -14,12 +28,13 @@ const BP: usize = 5;
 const SI: usize = 6;
 const DI: usize = 7;
 
+
 impl CPU {
     pub fn new() -> CPU {
         CPU {
             pc: 0,
             memory: [0; 0x10000],
-            r16: [0, 0, 0, 0, 0, 0, 0, 0],
+            r16: [Register16 { hi: 0, lo: 0 }; 8],
         }
     }
 
@@ -51,29 +66,42 @@ impl CPU {
             //0x48...0x4F => format!("dec {}", r16(b & 7)),
             0xB0...0xB7 => {
                 let val = self.read_u8();
-                let reg = (b&7) as usize;
-                println!("XXX IMPL: mov {}, {:04X}", reg, val);
+                let reg = (b & 7) as usize;
 
-                // XXX r8 which is pointers into the r16 struct ....
-                println!("mov {}, {:02X}", b & 7, val);
-            },
+                let lor = reg & 3;
+                if reg & 4 == 0 {
+                    self.r16[lor].lo = val;
+                } else {
+                    self.r16[lor].hi = val;
+                }
+            }
             0xB8...0xBF => {
                 // mov r16, u16
-                let val = self.read_u16();
-                let reg = (b&7) as usize;
-                self.r16[reg] = val;
-            },
+                let reg = (b & 7) as usize;
+                self.r16[reg].lo = self.read_u8();
+                self.r16[reg].hi = self.read_u8();
+            }
             0xCD => {
                 // XXX jump to offset 0x21 in interrupt table (look up how hw does this)
                 println!("XXX IMPL: int {:02X}", self.read_u8());
-            },
-            _ => println!("UNHANDLED OP {:02X} AT {:04X}", b, self.pc-1),
+            }
+            _ => println!("UNHANDLED OP {:02X} AT {:04X}", b, self.pc - 1),
         };
     }
 
     pub fn print_registers(&mut self) {
-        print!("pc:{:04X}  ax:{:04X} bx:{:04X} cx:{:04X} dx:{:04X}", self.pc, self.r16[AX], self.r16[BX], self.r16[CX], self.r16[DX]);
-        println!("  sp:{:04X} bp:{:04X} si:{:04X} di:{:04X}",  self.r16[SP], self.r16[BP], self.r16[SI], self.r16[DI]);    }
+        print!("pc:{:04X}  ax:{:04X} bx:{:04X} cx:{:04X} dx:{:04X}",
+               self.pc,
+               self.r16[AX].u16(),
+               self.r16[BX].u16(),
+               self.r16[CX].u16(),
+               self.r16[DX].u16());
+        println!("  sp:{:04X} bp:{:04X} si:{:04X} di:{:04X}",
+                 self.r16[SP].u16(),
+                 self.r16[BP].u16(),
+                 self.r16[SI].u16(),
+                 self.r16[DI].u16());
+    }
 
     fn read_u8(&mut self) -> u8 {
         let b = self.memory[self.pc as usize];
