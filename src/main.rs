347b2e80fb32f45c3de96d3f39b9cc911dba37d7
Martin Lindhe <martin-commit@ubique.se>
2017-03-24 01:23:33 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 #![allow(dead_code)]
 
-use std::fs::File;
-use std::io::Read;
-use std::process::exit;
-
 mod cpu;
-
+mod tools;
 
 fn main() {
 
     // XXX: /Users/m/dev/binary-samples/Executables/DOS-COM/
-    let data = read_binary("samples/hellodos/hello.com");
-    println!("{}", to_hex_string(&data));
+    let data = tools::read_binary("samples/hellodos/hello.com");
+    println!("{}", tools::to_hex_string(&data));
 
     let mut cpu = cpu::CPU::new();
 
@@ -22,32 +18,4 @@ fn main() {
         let disasm = cpu.disasm_instruction();
         println!("{}", disasm);
     }
-}
-
-
-pub fn read_binary(path: &str) -> Vec<u8> {
-    let mut buffer: Vec<u8> = Vec::new();
-
-    let mut f = match File::open(path) {
-        Ok(x) => x,
-        Err(why) => {
-            println!("Could not open file {}: {}", path, why);
-            exit(1);
-        }
-    };
-
-    match f.read_to_end(&mut buffer) {
-        Ok(x) => x,
-        Err(why) => {
-            println!("could not read contents of file: {}", why);
-            exit(1);
-        }
-    };
-
-    buffer
-}
-
-pub fn to_hex_string(bytes: &Vec<u8>) -> String {
-    let strs: Vec<String> = bytes.iter().map(|b| format!("{:02X}", b)).collect();
-    strs.join(" ")
 }
---
+++ src/tools.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::Read;
+use std::process::exit;
+
+pub fn read_binary(path: &str) -> Vec<u8> {
+    let mut buffer: Vec<u8> = Vec::new();
+
+    let mut f = match File::open(path) {
+        Ok(x) => x,
+        Err(why) => {
+            println!("Could not open file {}: {}", path, why); // XXX return error to caller.. how?!11 so they can call .except() ..=?!?1
+            exit(1);
+        }
+    };
+
+    match f.read_to_end(&mut buffer) {
+        Ok(x) => x,
+        Err(why) => {
+            println!("could not read contents of file: {}", why);
+            exit(1);
+        }
+    };
+
+    buffer
+}
+
+pub fn to_hex_string(bytes: &Vec<u8>) -> String {
+    let strs: Vec<String> = bytes.iter().map(|b| format!("{:02X}", b)).collect();
+    strs.join(" ")
+}
