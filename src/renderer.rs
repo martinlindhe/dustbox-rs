791c9c3b377aeafa7c8cd8a7556e39d9a2e653a3
Martin Lindhe <martin-commit@ubique.se>
2017-06-12 18:01:37 +0200

    ugly but working victory

--- src/renderer.rs
+++ src/renderer.rs
@@ -23,107 +23,118 @@ pub fn main() {
     let mut window = Window::new(Rect::new(0, 0, WIDTH, HEIGHT), "x86emu");
 
 
-    let app = Arc::new(Mutex::new(debugger::Debugger::new()));
-
+    let app = Arc::new(Mutex::new(debugger::Debugger::new())); // XXX
     let x = 10;
     let y = 10;
 
-    let canvas = Image::from_color(320, 200, Color::rgb(0, 0, 0));
-    canvas.position(WIDTH as i32 - 340, 10);
-    window.add(&canvas);
+    let canvas = Arc::new(Mutex::new(Image::from_color(320, 200, Color::rgb(0, 0, 0)))); // XXX can the canvas live in the GPU struct?
+    let canvas_copy = canvas.clone();
+    canvas_copy.lock().unwrap().position(WIDTH as i32 - 340, 10);
+    window.add(&canvas_copy.lock().unwrap());
 
 
     let disasm_text = app.lock().unwrap().disasm_n_instructions_to_text(20);
     let reg_text = app.lock().unwrap().cpu.print_registers();
 
-    let disasm = Label::new();
-    disasm.position(x, y).size(450, 20 * 20).text(disasm_text);
-    window.add(&disasm);
+    let disasm = Arc::new(Mutex::new(Label::new()));
+    let disasm_copy = disasm.clone();
+    disasm_copy.lock().unwrap().position(x, y).size(450, 20 * 20).text(disasm_text);
+    window.add(&disasm_copy.lock().unwrap());
 
-    let regs = Label::new();
-    regs.position(WIDTH as i32 - 300, 300)
+    let regs = Arc::new(Mutex::new(Label::new()));
+    let regs_copy = regs.clone();
+    regs_copy.lock().unwrap().position(WIDTH as i32 - 300, 300)
         .size(290, 80)
         .text(reg_text);
-    window.add(&regs);
+    window.add(&regs_copy.lock().unwrap());
 
     let button_width = 90;
     let pad = 10;
 
-    let btn_step = Button::new();
-    btn_step
-        .position(x, HEIGHT as i32 - 50)
-        .size(button_width, 30)
-        .text("Step into")
-        .text_offset(6, 6)
-        .on_click(move |_button: &Button, _point: Point| {
 
-            let mut dbg = app.lock().unwrap();
+        let step_copy = app.clone();
+        let disasm_step_copy = disasm.clone();
+        let regs_step_copy = regs.clone();
+        let canvas_step_copy = canvas.clone();
 
-            dbg.cpu.fatal_error = false;
+        let btn_step = Button::new();
+        btn_step
+            .position(x, HEIGHT as i32 - 50)
+            .size(button_width, 30)
+            .text("Step into")
+            .text_offset(6, 6)
+            .on_click(move |_button: &Button, _point: Point| {
 
-            dbg.step_into();
+                let mut shared = step_copy.lock().unwrap();
 
-            // update disasm
-            let disasm_text = dbg.disasm_n_instructions_to_text(20);
-            disasm.text(disasm_text);
+                shared.cpu.fatal_error = false;
+                shared.step_into();
 
-            // update regs
-            let reg_text = dbg.cpu.print_registers();
-            regs.text(reg_text);
+                // update disasm
+                let disasm_text = shared.disasm_n_instructions_to_text(20);
+                disasm_step_copy.lock().unwrap().text(disasm_text);
 
-            render_canvas(&canvas, &dbg.cpu);
-        });
-    window.add(&btn_step);
+                // update regs
+                let reg_text = shared.cpu.print_registers();
+                regs_step_copy.lock().unwrap().text(reg_text);
 
-/*
-    let btn_step2 = Button::new();
-    btn_step2
-        .position(x + button_width as i32 + pad, HEIGHT as i32 - 50)
-        .size(button_width, 30)
-        .text("Step over")
-        .text_offset(6, 6)
-        .on_click(move |_button: &Button, _point: Point| {
+                render_canvas(&canvas_step_copy.lock().unwrap(), &shared.cpu);
+            });
+        window.add(&btn_step);
 
-            let mut dbg = app.lock().unwrap();
 
-            dbg.cpu.fatal_error = false;
+        let step2_copy = app.clone();
+        let disasm_step2_copy = disasm.clone();
+        let regs_step2_copy = regs.clone();
+        let canvas_step2_copy = canvas.clone();
 
-            dbg.step_over();
+        let btn_step2 = Button::new();
+        btn_step2
+            .position(x + button_width as i32 + pad, HEIGHT as i32 - 50)
+            .size(button_width, 30)
+            .text("Step over")
+            .text_offset(6, 6)
+            .on_click(move |_button: &Button, _point: Point| {
 
-            // update disasm
-            let disasm_text = dbg.disasm_n_instructions_to_text(20);
-            disasm.text(disasm_text);
+                let mut shared = step2_copy.lock().unwrap();
 
-            // update regs
-            let reg_text = dbg.cpu.print_registers();
-            regs.text(reg_text);
+                shared.cpu.fatal_error = false;
+                shared.step_over();
 
-            render_canvas(&canvas, &dbg.cpu);
-        });
-    window.add(&btn_step2);
-*/
+                // update disasm
+                let disasm_text = shared.disasm_n_instructions_to_text(20);
+                disasm_step2_copy.lock().unwrap().text(disasm_text);
+
+                // update regs
+                let reg_text = shared.cpu.print_registers();
+                regs_step2_copy.lock().unwrap().text(reg_text);
+
+                render_canvas(&canvas_step2_copy.lock().unwrap(), &shared.cpu);
+            });
+        window.add(&btn_step2);
+
+
     window.exec();
 }
 
 fn render_canvas(canvas: &std::sync::Arc<orbtk::Image>, cpu: &CPU) {
-    // draw on img
-            let mut image = canvas.image.borrow_mut();
+    let mut image = canvas.image.borrow_mut();
 
-            // XXX rather replace image pixels
-            // image = dbg.cpu.gpu.render_frame();
-            // image.from_data(frame.into_data());
+    // XXX rather replace image pixels
+    // image = dbg.cpu.gpu.render_frame();
+    // image.from_data(frame.into_data());
 
-            // VGA, mode 13h:
-            let height = 320; // dbg.cpu.gpu.height;
-            let width = 240; // dbg.cpu.gpu.width;
+    // VGA, mode 13h:
+    let height = 320; // dbg.cpu.gpu.height;
+    let width = 240; // dbg.cpu.gpu.width;
 
-            for y in 0..height {
-                for x in 0..width {
-                    let offset = 0xA0000 + ((y * width) + x) as usize;
-                    let byte = cpu.memory.memory[offset];
-                    let pal = &cpu.gpu.palette[byte as usize];
-                    image.pixel(x as i32, y as i32, Color::rgb(pal.r, pal.g, pal.b));
-                }
-            }
+    for y in 0..height {
+        for x in 0..width {
+            let offset = 0xA0000 + ((y * width) + x) as usize;
+            let byte = cpu.memory.memory[offset];
+            let pal = &cpu.gpu.palette[byte as usize];
+            image.pixel(x as i32, y as i32, Color::rgb(pal.r, pal.g, pal.b));
+        }
+    }
 }
 
