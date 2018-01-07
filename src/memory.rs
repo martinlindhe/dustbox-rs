89deb17e9335aad1947ece5906de810a58b023ce
Nicklas Warming Jacobsen <nicklaswj@gmail.com>
2018-01-07 14:30:04 +0100

    Added read/write functions to memory

--- src/memory.rs
+++ src/memory.rs
@@ -7,4 +7,22 @@ impl Memory {
     pub fn new() -> Self {
         Memory { memory: vec![0u8; 0x1_0000 * 64] }
     }
+
+    pub fn read_u8(self, addr: usize) -> u8 {
+        self.memory[addr]
+    }
+
+    pub fn read_u16(self, addr: usize) -> u16 {
+        u16::from(self.read_u8(addr)) << 8 |
+            u16::from(self.read_u8(addr+1))
+    }
+
+    pub fn write_u8(self, addr: usize, data: u8) {
+        self.memory[addr] = data;
+    }
+
+    pub fn write_u16(self, addr: usize, data: u16) {
+        self.write_u8(addr, (data & 0xff) as u8);
+        self.write_u8(addr+1, (data >> 8) as u8);
+    }
 }
