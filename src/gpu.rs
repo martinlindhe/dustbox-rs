afbb3645ddb1f0ad5f4e6f1fddf04ea05b5035c1
Martin Lindhe <martin-commit@ubique.se>
2018-01-27 15:17:39 +0100

    gpu: initialize the ega palette

--- src/gpu.rs
+++ src/gpu.rs
@@ -24,11 +24,29 @@ pub struct DACPalette {
 
 impl GPU {
     pub fn new() -> Self {
+        let mut pal = vec![DACPalette { r: 0, g: 0, b: 0 }; 256];
+        // the standard EGA palette
+        pal[0]  = DACPalette {r: 0, g: 0, b: 0};       // black
+        pal[1]  = DACPalette {r: 0, g: 0, b: 170};     // blue
+        pal[2]  = DACPalette {r: 0, g: 170, b: 0};     // green
+        pal[3]  = DACPalette {r: 0, g: 170, b: 170};   // cyan
+        pal[4]  = DACPalette {r: 170, g: 0, b: 0};     // red
+        pal[5]  = DACPalette {r: 170, g: 0, b: 170};   // magenta
+        pal[6]  = DACPalette {r: 170, g: 85, b: 0};    // brown
+        pal[7]  = DACPalette {r: 170, g: 170, b: 170}; // gray
+        pal[8]  = DACPalette {r: 85, g: 85, b: 85};    // dark gray
+        pal[9]  = DACPalette {r: 85, g: 85, b: 255};   // bright blue
+        pal[10] = DACPalette {r: 85, g: 255, b: 85};   // bright green
+        pal[11] = DACPalette {r: 85, g: 255, b: 255};  // bright cyan
+        pal[12] = DACPalette {r: 255, g: 85, b: 85};   // bright red
+        pal[13] = DACPalette {r: 255, g: 85, b: 255};  // bright magenta
+        pal[14] = DACPalette {r: 255, g: 255, b: 85};  // yellow
+        pal[15] = DACPalette {r: 255, g: 255, b: 255}; // white
         GPU {
             scanline: 0,
             width: 320,
             height: 200,
-            pal: vec![DACPalette { r: 0, g: 0, b: 0 }; 256],
+            pal: pal,
             dac_color: 0,
             dac_index: 0,
             dac_current_pal: vec![0u8; 3],
